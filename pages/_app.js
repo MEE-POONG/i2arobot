b22d1390d32999dd2e3c1e309013dd40f67b4d55
devilzeros <devilzeros@outlook.co.th>
2022-09-07 14:22:27 +0700

    est

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// import '../styles/css/globals.css';
+// import '../styles/scss/index.scss';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
+import { RecoilRoot } from 'recoil';
+import Layout from '../layouts/IndexPage';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </SSRProvider>
+
+  );
 }
 
-export default MyApp
+export default MyApp;
