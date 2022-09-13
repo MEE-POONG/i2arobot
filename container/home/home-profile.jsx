544afa981c7f9e32270a12ae81c8fdae16461aca
devilzeros <devilzeros@outlook.co.th>
2022-09-13 13:21:27 +0700

    est

--- container/home/home-profile.jsx
+++ container/home/home-profile.jsx
@@ -9,7 +9,7 @@ export default function HomeProfile() {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 1
     };
     return (
         <>
@@ -30,8 +30,19 @@ export default function HomeProfile() {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-lg-12">
-                            <div className="owl-carousel owl-portfolio">
-                                <div className="item">
+                            <Slider {...settings}>
+                                <Card className="owl-item item">
+                                    <div className="thumb">
+                                        <Image src="images/portfolio-01.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
+                                                <span>Plot Listing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Card>
+                                <div className="owl-item item">
                                     <div className="thumb">
                                         <Image src="images/portfolio-01.jpg" alt="" />
                                         <div className="hover-effect">
@@ -42,130 +53,29 @@ export default function HomeProfile() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-02.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>Project Two</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-03.jpg" alt="" />
-                                        <Image src={'images/about-left-image.png'} alt="Two Girls working together" />
-
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a rel="sponsored" href="https://templatemo.com/tm-562-space-dynamic" target="_parent"><h4>Third Project</h4></a>
-                                                <span>Space Dynamic SEO</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-04.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>Project Four</h4></a>
-                                                <span>Website Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
+                                <div className="owl-item item">
                                     <div className="thumb">
                                         <Image src="images/portfolio-01.jpg" alt="" />
                                         <div className="hover-effect">
                                             <div className="inner-content">
-                                                <a href="#"><h4>Fifth Project</h4></a>
-                                                <span>Digital Assets</span>
+                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
+                                                <span>Plot Listing</span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-02.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>Sixth Project</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-03.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>7th Project</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-04.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>8th Project</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
+                                <div className="owl-item item">
                                     <div className="thumb">
                                         <Image src="images/portfolio-01.jpg" alt="" />
                                         <div className="hover-effect">
                                             <div className="inner-content">
-                                                <a href="#"><h4>9th Project</h4></a>
-                                                <span>SEO &amp; Marketing</span>
+                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
+                                                <span>Plot Listing</span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-02.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>Project Ten</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-03.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>Project Eleven</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-04.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a href="#"><h4>12th Project</h4></a>
-                                                <span>SEO &amp; Marketing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            </Slider>
                         </div>
                     </div>
                 </div>
