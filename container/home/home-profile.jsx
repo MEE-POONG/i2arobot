362a37cc0a1ad3999c9d086da00ec52dadebe2db
devilzeros <devilzeros@outlook.co.th>
2022-09-29 18:32:41 +0700

    est

--- container/home/home-profile.jsx
+++ container/home/home-profile.jsx
@@ -31,46 +31,123 @@ export default function HomeProfile() {
                     <div className="row">
                         <div className="col-lg-12">
                             <Slider {...settings}>
-                                <Card className="owl-item item">
+                                <div className="item profile">
                                     <div className="thumb">
-                                        <Image src="images/portfolio-01.jpg" alt="" />
+                                        <Image src="./images/portfolio-02.jpg" alt="" />
                                         <div className="hover-effect">
                                             <div className="inner-content">
-                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
-                                                <span>Plot Listing</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Card>
-                                <div className="owl-item item">
-                                    <div className="thumb">
-                                        <Image src="images/portfolio-01.jpg" alt="" />
-                                        <div className="hover-effect">
-                                            <div className="inner-content">
-                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
-                                                <span>Plot Listing</span>
+                                                <a href="#"><h4>Project Two</h4></a>
+                                                <span>SEO &amp; Marketing</span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="owl-item item">
+                                <div className="item profile">
                                     <div className="thumb">
-                                        <Image src="images/portfolio-01.jpg" alt="" />
+                                        <Image src="./images/portfolio-03.jpg" alt="" />
                                         <div className="hover-effect">
                                             <div className="inner-content">
-                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
-                                                <span>Plot Listing</span>
+                                                <a rel="sponsored" href="https://templatemo.com/tm-562-space-dynamic" target="_parent"><h4>Third Project</h4></a>
+                                                <span>Space Dynamic SEO</span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="owl-item item">
+                                <div className="item profile">
                                     <div className="thumb">
-                                        <Image src="images/portfolio-01.jpg" alt="" />
+                                        <Image src="./images/portfolio-04.jpg" alt="" />
                                         <div className="hover-effect">
                                             <div className="inner-content">
-                                                <a rel="sponsored" href="https://templatemo.com/tm-564-plot-listing" target="_parent"><h4>First Project</h4></a>
-                                                <span>Plot Listing</span>
+                                                <a href="#"><h4>Project Four</h4></a>
+                                                <span>Website Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-01.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>Fifth Project</h4></a>
+                                                <span>Digital Assets</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-02.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>Sixth Project</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-03.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>7th Project</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-04.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>8th Project</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-01.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>9th Project</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-02.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>Project Ten</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-03.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>Project Eleven</h4></a>
+                                                <span>SEO &amp; Marketing</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item profile">
+                                    <div className="thumb">
+                                        <Image src="./images/portfolio-04.jpg" alt="" />
+                                        <div className="hover-effect">
+                                            <div className="inner-content">
+                                                <a href="#"><h4>12th Project</h4></a>
+                                                <span>SEO &amp; Marketing</span>
                                             </div>
                                         </div>
                                     </div>
@@ -79,7 +156,7 @@ export default function HomeProfile() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     );
 }
