645586ab1f2d40fd2e3796711eb4823d71278c56
lChikapul <Chipao1003@gmail.com>
2022-10-06 20:37:22 +0700

    contact-us

---
+++ container/home/contact-us.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Image, Carousel, Button, Card } from 'react-bootstrap';
+import { FaRegIdCard } from 'react-icons/fa';
+
+export default function contactus() {
+
+    return (
+            <>
+            </>
+    );
+}
