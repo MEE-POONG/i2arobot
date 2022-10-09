6462e0154a50c6e8afe241c2b5b0ac89e3979a4e
Carter <114277619+Carter44243@users.noreply.github.com>
2022-10-09 17:25:37 +0700

    viewvin

--- container/home/home-video.jsx
+++ container/home/home-video.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Image, Carousel, Button, Nav, Tab, Row, Col } from 'react-bootstrap';
+import { Image, Carousel, Button, Nav, Tab, Row, Col, Modal } from 'react-bootstrap';
 import { FaRegIdCard } from 'react-icons/fa';
 import Slider from "react-slick";
+
 
 export default function HomeVideo() {
     const settings = {
@@ -11,6 +12,7 @@ export default function HomeVideo() {
         slidesToShow: 3,
         slidesToScroll: 3
     };
+    const [viewVideo, setViewVideo] = useState({ show: false, video: "", fourthtopic: "", contentfour: "" })
     return (
         <>
             <div id="video" className="our-videos section">
@@ -20,16 +22,16 @@ export default function HomeVideo() {
                 <div className="videos-right-dec">
                     <Image src="./images/videos-right-dec.png" alt="" />
                 </div>
-                <div className="container">
+                <div className="container d-lg-block d-none">
                     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                         <div className="naccs">
-                            <div className="grid">
+                            <div className="grid ">
                                 <Row>
-                                    <Col lg="8">
+                                    <Col lg="8" md="9">
                                         <Tab.Content className="nacc">
                                             <Tab.Pane eventKey="first">
                                                 <div className="thumb">
-                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/5ioJMT6nsbc" title="รถตัดหญ้าบังคับวิทยุ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/5ioJMT6nsbc" title="ศักยภาพในการตัดหญ้า" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                                                     <div className="overlay-effect">
                                                         <a href="#"><h4>Potential</h4></a>
                                                         <span>ศักยภาพในการตัดหญ้า</span>
@@ -38,7 +40,7 @@ export default function HomeVideo() {
                                             </Tab.Pane>
                                             <Tab.Pane eventKey="second">
                                                 <div className="thumb">
-                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/Q6LyqP4j7_o" title="14 กรกฎาคม ค.ศ. 2022" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/Q6LyqP4j7_o" title="ความแข็งแรงทนทานในการใช้งาน" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                                                     <div className="overlay-effect">
                                                         <a href="#"><h4>Strong</h4></a>
                                                         <span>ความแข็งแรงทนทานในการใช้งาน</span>
@@ -47,7 +49,7 @@ export default function HomeVideo() {
                                             </Tab.Pane>
                                             <Tab.Pane eventKey="third">
                                                 <div className="thumb">
-                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/V9EILNde0cg" title="รถตัดหญ้างบังคับวิทยุ 13แรง ใบ80cm" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/V9EILNde0cg" title="ง่ายต่อการควมคุม" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                                                     <div className="overlay-effect">
                                                         <a href="#"><h4>Control</h4></a>
                                                         <span>ง่ายต่อการควมคุม</span>
@@ -56,7 +58,7 @@ export default function HomeVideo() {
                                             </Tab.Pane>
                                             <Tab.Pane eventKey="fourth">
                                                 <div className="thumb">
-                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/dJl9LG-4FV0" title="สเปคเบื้องต้นรถตัดหญ้าบังคับวิทยุ ไอ.โอดีไซน์" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/dJl9LG-4FV0" title="สเปคเบื้องต้นรถตัดหญ้าบังคับวิทยุ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                                                     <div className="overlay-effect">
                                                         <a href="#"><h4>Spec</h4></a>
                                                         <span>สเปคเบื้องต้นรถตัดหญ้าบังคับวิทยุ</span>
@@ -65,7 +67,7 @@ export default function HomeVideo() {
                                             </Tab.Pane>
                                         </Tab.Content>
                                     </Col>
-                                    <Col lg="4">
+                                    <Col lg="4" md="3">
                                         <div className="menu">
                                             <Nav variant="pills" className="flex-column">
                                                 <Nav.Item >
@@ -112,28 +114,94 @@ export default function HomeVideo() {
                         </div>
                     </Tab.Container>
                 </div>
-            </div>
-            {/* <div id="subscribe" className="subscribe">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="inner-content">
-                                <div className="row">
-                                    <div className="col-lg-10 offset-lg-1">
-                                        <h2>Know Your Website SEO Score by Email</h2>
-                                        <form id="subscribe" action="" method="get">
-                                            <input type="text" name="website" id="website" placeholder="Your Website URL" required="" />
-                                            <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email"
-                                                required="" />
-                                            <button type="submit" id="form-submit" className="main-button ">Subscribe</button>
-                                        </form>
-                                    </div>
-                                </div>
+                <div className="container d-md-block d-lg-none">
+                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+
+                        <div className="naccs">
+                            <div className="menu">
+                                <Row>
+                                    <Col md={6}>
+                                        <Nav.Item >
+                                            <Nav.Link eventKey="first" className='thumb p-0' onClick={() => setViewVideo({ ...viewVideo, show: true, video: "https://www.youtube.com/embed/5ioJMT6nsbc", fourthtopic: "Potential", contentfour: "ศักยภาพในการตัดหญ้า" })}>
+                                                <Image src="./images/video-thumb-07.png" alt="" />
+                                                <div className="inner-content">
+                                                    <h4>Potential</h4>
+                                                    <span>ศักยภาพในการตัดหญ้า</span>
+                                                </div>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                    </Col>
+                                    <Col md={6}>
+                                        <Nav.Item >
+                                            <div class="nav-item">
+                                                
+
+                                                    <Nav.Link eventKey="first" className='thumb p-0' onClick={() => setViewVideo({ ...viewVideo, show: true, video: "https://www.youtube.com/embed/Q6LyqP4j7_o", fourthtopic: "Strong", contentfour: "ความแข็งแรงทนทานในการใช้งาน" })}>
+                                                        <Image src="./images/video-thumb-01.png" alt="" />
+                                                        <div className="inner-content">
+                                                            <h4>Strong</h4>
+                                                            <span>ความแข็งแรงทนทานในการใช้งาน</span>
+                                                        </div>
+
+                                                    </Nav.Link>
+                                               
+                                            </div>
+
+                                        </Nav.Item>
+                                    </Col>
+                                    <Col md={6}>
+                                        <Nav.Item >
+                                            <Nav.Link eventKey="first" className='thumb p-0' onClick={() => setViewVideo({ ...viewVideo, show: true, video: "https://www.youtube.com/embed/V9EILNde0cg", fourthtopic: "Control", contentfour: "ง่ายต่อการควมคุม" })}>
+                                                <Image src="./images/video-thumb-02.png" alt="" />
+                                                <div className="inner-content">
+                                                    <h4>Control</h4>
+                                                    <span>ง่ายต่อการควมคุม</span>
+                                                </div>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                    </Col>
+                                    <Col md={6}>
+                                        <Nav.Item >
+                                            <Nav.Link eventKey="first" className='thumb p-0' onClick={() => setViewVideo({ ...viewVideo, show: true, video: "https://www.youtube.com/embed/dJl9LG-4FV0", fourthtopic: "Spec", contentfour: "สเปคเบื้องต้นรถตัดหญ้าบังคับวิทยุ" })}>
+                                                <Image src="./images/video-thumb-08.png" alt="" />
+                                                <div className="inner-content">
+                                                    <h4>Spec</h4>
+                                                    <span>สเปคเบื้องต้นรถตัดหญ้าบังคับวิทยุ</span>
+                                                </div>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                    </Col>
+
+                                </Row>
+
                             </div>
                         </div>
-                    </div>
+                        <Modal
+                            size="md"
+                            fullscreen={true}
+                            centered
+                            show={viewVideo.show}
+                            onHide={() => setViewVideo({ ...viewVideo, show: false, video: "" })}
+                            aria-labelledby="example-modal-sizes-title-sm"
+                        >
+                            <Modal.Header closeButton>
+                                <Modal.Title id="example-custom-modal-styling-title">
+                                    <h4>{viewVideo.fourthtopic}</h4>
+
+
+                                    <span>{viewVideo.contentfour}</span>
+
+                                </Modal.Title>
+                            </Modal.Header>
+                            <iframe
+                                style={{ height: "inherit" }}
+                                src={viewVideo.video} title="รถตัดหญ้าบังคับวิทยุ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </Modal>
+                    </Tab.Container>
+
                 </div>
-            </div> */}
+            </div >
+
 
 
         </>
