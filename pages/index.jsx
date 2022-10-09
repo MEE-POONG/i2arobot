931e601c3f115240b03ff486dadbb45c78b7a181
Carter <114277619+Carter44243@users.noreply.github.com>
2022-10-09 13:00:12 +0700

    vviewvin

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,8 +28,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        {/* <SlideHome /> */}
-        <OurTools />
+        <SlideHome />
+        {/* <OurTools /> */}
         {/* <HomeAbout /> */}
         {/* <HomeProfile /> */}
         {/* <HomePricing /> */}
