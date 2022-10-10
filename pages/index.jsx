f8933397698cd1bf3e75382ab29b11dbd4bf2d70
lChikapul <Chipao1003@gmail.com>
2022-10-10 15:25:39 +0700

    bt

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ import SlideHome from 'container/home/home-banner';
 import OurTools from 'container/home/our-tools';
 import HomeProfile from 'container/home/home-profile';
 import HomePricing from 'container/home/home-pricing';
-
+import Homenext from 'container/home/home-next';
 
 
 import HomeAbout from 'container/home/home-about';
@@ -27,12 +27,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        {/* <SlideHome /> */}
+        <SlideHome />
         {/* <OurTools /> */}
-        {/* <HomeAbout /> */}
+        <HomeAbout />
         {/* <HomeProfile /> */}
         {/* <HomePricing /> */}
-        <HomeVideo />
+        {/* <HomeVideo /> */}
+        <Homenext />
       </div>
     </ >
   );
