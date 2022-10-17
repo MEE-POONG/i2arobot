3d1a99ee688e30c36594244e106b3ead644a940a
ChunWarayut <warayut2555@gmail.com>
2022-10-17 23:38:29 +0700

    บริษัท มี พร้อมท์ เทคโนโลยี จำกัด

--- components/Footers/Footer.jsx
+++ components/Footers/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image } from 'react-bootstrap';
+import { Image } from "react-bootstrap";
 import { FaBehance, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
@@ -12,13 +12,14 @@ export default function Footer() {
               <div className="about footer-item">
                 <div className="logo">
                   <a href="#">
-                    <Image src={'images/iodesign.png'} alt="logo-nav" />
+                    <Image src={"images/iodesign.png"} alt="logo-nav" />
                   </a>
                 </div>
-                <a href="#">© 2022 I.O.Design & Home Co., Ltd.
-                  บริษัท ไอ.โอ.ดีไซน์ แอนด์ โฮม จำกัด
-                  All Rights Reserved</a>
-                  
+                <a href="#">
+                  © 2022 I.O.Design & Home Co., Ltd. บริษัท ไอ.โอ.ดีไซน์ แอนด์
+                  โฮม จำกัด All Rights Reserved
+                </a>
+
                 <ul>
                   <li>
                     <a href="#">
@@ -58,7 +59,6 @@ export default function Footer() {
                   <p></p>
                   <p></p>
                   <p> </p>
-
                 </ul>
               </div>
             </div>
@@ -68,32 +68,54 @@ export default function Footer() {
                 <ul>
                   <p> </p>
                   <p></p>
-
                 </ul>
               </div>
             </div>
             <div className="col-lg-3">
               <div className="subscribe-newsletters footer-item">
                 <h4>บริษัท ไอ.โอ.ดีไซน์ แอนด์ โฮม จำกัด</h4>
-                <p>21/1 ซอยอนามัยงามเจริญ 25 แยก 2-7 แขวงท่าข้าม เขตบางขุนเทียน กรุงเทพฯ 10150</p>
+                <p>
+                  21/1 ซอยอนามัยงามเจริญ 25 แยก 2-7 แขวงท่าข้าม เขตบางขุนเทียน
+                  กรุงเทพฯ 10150
+                </p>
                 <p>TEL : 02-409-2152-9</p>
                 <p>FAX : 02-409-2150-1</p>
                 <p> Hot Line : 091-739-3027</p>
                 <p> EMAIL:oiio2005@yahoo.co.th</p>
                 <p>EMAIL:oiio2005@hotmail.co.th</p>
                 <form action="#" method="get">
-                  <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required="" />
-                  <button type="submit" id="form-submit" className="main-button "><i className="fa fa-paper-plane-o"></i></button>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    pattern="[^ @]*@[^ @]*"
+                    placeholder="Your Email"
+                    required=""
+                  />
+                  <button
+                    type="submit"
+                    id="form-submit"
+                    className="main-button "
+                  >
+                    <i className="fa fa-paper-plane-o"></i>
+                  </button>
                 </form>
               </div>
             </div>
             <div className="col-lg-12">
               <div className="copyright">
-                <p>ME PROMPT TECHNOLOGY COMPANY LIMITED
+                <p>
+                  ME PROMPT TECHNOLOGY COMPANY LIMITED
                   <br />
-                  บริษัท มี พุง กรุ๊ป จำกัด <a rel="nofollow" href="https://www.meepoong.com/" title="free CSS templates">WRDSITE MEE POONG</a>
+                  
+                  <a
+                    rel="nofollow"
+                    href="https://www.meepoong.com/"
+                    title="free CSS templates"
+                  >
+                    บริษัท มี พร้อมท์ เทคโนโลยี จำกัด
+                  </a>
                   <br />
-                
                 </p>
               </div>
             </div>
